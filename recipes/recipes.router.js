3dfb1f2445a2d39c612c886064ea7de7e6535a32
Wellington Junior <wellington@MacBook-Pro-de-Wellington.local>
2020-11-28 14:11:49 -0300

    criando rota de recipes

--- recipes/recipes.router.js
+++ recipes/recipes.router.js
@@ -1,3 +1,15 @@
-module.exports = {
-    
+const RecipesRouter = {
+
+    //metodo para obter dados dos serviços, giphy, recipePuppy
+    getServices: async (req, res, next)=>{
+        res.json({menssage: 'Lista de Receitas' });
+    },
+
+    //todas rotas de recipes
+    applyRouter: (application) => {
+        //declaracao de todas as rotas 
+        application.get(`/recipes/`, [RecipesRouter.getServices]);
+    }
 }
+
+module.exports = RecipesRouter;
