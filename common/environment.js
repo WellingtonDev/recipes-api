c5808a617d514ff998da3ff99ff6e3a0ffc5c070
Wellington Junior <wellington@MacBook-Pro-de-Wellington.local>
2020-11-28 12:07:14 -0300

    enviroment para configurações e APIs externas

---
+++ common/environment.js
@@ -0,0 +1,14 @@
+module.exports = {
+    server: {
+        port: process.env.SERVER_PORT || 3000
+    },
+    services: {
+        recipePuppy: {
+            url: 'http://www.recipepuppy.com/api/?i='
+        },
+        giphy: {
+            apiKey: 'iNY1TYNTfXKb2DEtYLUeq06YvDuqy42f',
+            url: 'http://api.giphy.com/v1/gifs/search'
+        }
+    }
+};
