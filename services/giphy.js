c3c29d0eaac1d65f1d0f382742cb2f98fe211fcd
Wellington Junior <wellington@MacBook-Pro-de-Wellington.local>
2020-11-28 21:35:25 -0300

    correçao de giphy

--- services/giphy.js
+++ services/giphy.js
@@ -5,6 +5,6 @@ module.exports = {
 
     //metodo de requisição get giphy
     getGiphy: (param) => {
-        return axios.get(`${environment.services.giphy.url}?api_key=${environment.services.giphy.apiKey}&q=${param}&limit=${environment.services.giphy.limit}`).then(response => response.data.data[0] );
+            return axios.get(`${environment.services.giphy.url}?api_key=${environment.services.giphy.apiKey}&q=${param}&limit=${environment.services.giphy.limit}`).then(response => response.data.data[0] );
     }
 }
