0d06939449817bb9172fc60e1a4b3fc0e90a69e3
Wellington Junior <wellington@MacBook-Pro-de-Wellington.local>
2020-11-28 21:38:16 -0300

    aplicaçao de logica de requisiçao

--- recipes/recipes.router.js
+++ recipes/recipes.router.js
@@ -1,13 +1,47 @@
+const giphy = require('../services/giphy');
+const recipePuppy = require('../services/recipePuppy');
+
 const RecipesRouter = {
 
-    //metodo para obter dados dos serviços, giphy, recipePuppy
+    //metodo para obter dados de serviços, giphy, recipePuppy
     getServices: async (req, res, next)=>{
-        res.json({menssage: 'Lista de Receitas' });
+        try
+        {
+            let params   = req.query.i;
+            //retorno de ingredientes
+            let response = await recipePuppy.getRecipePuppy(params);
+            //retorno de array de promessas, ingreditentes com giphy
+            let recipes  = response.results.map(async(ingredient)=>{
+            //buscando giphy com titulo do ingreditente
+            let result   = await giphy.getGiphy(ingredient.title);
+               return {
+                    title: ingredient.title,
+                    link: ingredient.href,
+                    ingredients: ingredient.ingredients.split(','),
+                    gif: result.url
+               }
+            });
+
+            let recipesFull = {
+                keywords: params.split(','),
+                recipes: []
+            }
+
+            Promise.all(recipes).then(result=>{
+                recipesFull.recipes = result
+                res.json(recipesFull)
+            });
+        }
+        catch(error)
+        {
+            console.error(error);
+            res.send(500, {mensage: "Estamos com instabilidade."});
+        }
     },
 
     //todas rotas de recipes
     applyRouter: (application) => {
-        //declaracao de todas as rotas 
+        //declaração de todas as rotas 
         application.get(`/recipes/`, [RecipesRouter.getServices]);
     }
 }
